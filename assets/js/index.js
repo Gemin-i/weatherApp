c7ae2fb315fa859ddbba3d00ba8012ca1de777eb
Gemin-i <tahirmahmudov2107@gmail.com>
2025-02-21 19:29:43 +0400

    added logo + api

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,23 @@
 let input = document.getElementById('input')
 let btn = document.getElementById('button')
-let weather = document.getElementById('description')
+let temp = document.getElementById('temp')
+let city = document.getElementById('city')
+let date = document.getElementById('date')
+let mainIcon = document.getElementById('mainIcon')
+
+
 
 btn.addEventListener('click', async function() {
+    weather()
+})
+
+input.addEventListener('keydown', async function(event) {
+    if (event.key === 'Enter') {
+        weather()
+    }
+})
+
+async function weather() {
     const countryName = input.value;
     console.log(countryName)
       try {
@@ -11,10 +26,32 @@ btn.addEventListener('click', async function() {
         );
         const data = await response.json();
         console.log(data)
-        weather.innerHTML = data.weather[0].icon
+        let temperature = Math.round(data.main.temp)
+        temp.textContent = temperature + "\u00B0"
+        mainIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        city.textContent = data.name
+        dateF(data)
+        
       } catch (error) {
         console.error(error);
       }
-})
-    
+}
 
+function dateF(data) {
+    let dateObj = new Date((data.dt * 1000) + (data.timezone * 1000))
+    let utcDate = dateObj.toUTCString()
+    let arr = utcDate.split(" ")
+    let days = {'Mon': 'Monday', 'Tue': 'Tuesday', 'Wed': 'Wednesday', 'Thu': 'Thursday', 'Fri': 'Friday', 'Sat': 'Saturday', 'Sun': 'Sunday'}  
+    let month = arr[2]
+    let monthDay = arr[1]
+    let year = arr[3]. slice(2, 4)
+    let time = arr[4].slice(0, 5)
+    let weekday = arr[0].slice(0, 3)
+    
+    if (days[weekday]) {
+        weekday = days[weekday]    
+    }
+
+    date.textContent = `${time} - ${weekday}, ${monthDay} ${month} '${year}`
+    
+}
